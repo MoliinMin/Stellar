7db9aea214c58a12392389dd2fa874f29fff5812
humin <1510112145@qq.com>
2022-01-24 17:37:25 +0800

    临时保存

--- stellar_runner.cc
+++ stellar_runner.cc
@@ -1,10 +1,23 @@
 #include "opencv2/opencv.hpp"
 #include "stellar.h"
-
+#include "data_types.h"
 
 int main()
 {
 
+	StellarParams stellar_params;
+	Stellar *stellar = Stellar::Create();
+	stellar->Initialize(stellar_params); 
+	stellar->StartDevice();
+	while (1)
+	{
+		cv::Mat depth_image, rgb_image;
+		stellar->GetFrames(depth_image, rgb_image);
+		stellar->StellarFusion(depth_image,rgb_image);
+		cv::waitKey(20);
+	}
+	stellar->DestoryDevice();
+	Stellar::Destory(stellar);
 
 	return 0;
 }
